a4ac4fb6c26827d2f0e73bbdd091d766dbd36d50
ReyolRhema <126499554+ReyolRhema@users.noreply.github.com>
2023-03-12 18:10:10 +0530

    Add files via upload

---
+++ stringEquals.java
@@ -0,0 +1,25 @@
+package week3.day2;
+
+public class stringEquals {
+	
+	public static void main(String[] args) {
+		//open a main method
+		//create a two strings to find whether the two string variables is equal or not
+		String name = "reyol";
+		String nameee ="Reyol";
+		//output:not equal(given variables are different)
+		// initiate if to check the string variables are equal
+				if(name.equals(nameee))
+					//use if equals method to find is that given variables are equal
+				System.out.println("is equal");
+				else {
+					System.out.println("not equal");
+					//print any one variable in if equals to check if it is equal if its not the initiate else 
+				}
+		
+	}
+
+}
+
+
+
